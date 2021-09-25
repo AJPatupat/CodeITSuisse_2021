optopt - fixed numerical analysis

codeitsuisse/routes/options.py
@@ -10,9 +10,6 @@ logger = logging.getLogger(__name__)
 from scipy.stats import norm
 from math import sqrt
 
-def x0_minus_x1_all_over_x2_minus_x3(x0, x1, x2, x3):
-    return (x0 * (1 - x1/x0)) / (x2 * (1 - x3/x2))
-
 def expected_return_per_view(option_dict, view_dict):
     strike = option_dict['strike']
     premium = option_dict['premium']
@@ -29,9 +26,12 @@ def expected_return_per_view(option_dict, view_dict):
             aa = (a-mu)/sigma
             bb = (b-mu)/sigma
             cc = (c-mu)/sigma
-            multiplier0 = x0_minus_x1_all_over_x2_minus_x3(norm.cdf(bb), norm.cdf(cc), norm.cdf(bb), norm.cdf(aa))
-            multiplier1 = x0_minus_x1_all_over_x2_minus_x3(norm.pdf(bb), norm.pdf(cc), norm.cdf(bb), norm.cdf(cc))
-            return multiplier0 * (mu - sigma * multiplier1 - strike) - premium
+            diff_cdf_bb_aa = norm.cdf(bb) - norm.cdf(aa)
+            diff_cdf_bb_cc = norm.cdf(bb) - norm.cdf(cc)
+            diff_pdf_bb_cc = norm.pdf(bb) - norm.pdf(cc)
+            multiplier0 = diff_cdf_bb_cc / diff_cdf_bb_aa
+            multiplier1 = diff_pdf_bb_cc / diff_cdf_bb_aa
+            return multiplier0 * (mu - strike) - multiplier1 * sigma - premium
 
     else:
         if strike <= view_dict['min']:
@@ -45,9 +45,12 @@ def expected_return_per_view(option_dict, view_dict):
             aa = (a-mu)/sigma
             bb = (b-mu)/sigma
             dd = (d-mu)/sigma
-            multiplier0 = x0_minus_x1_all_over_x2_minus_x3(norm.cdf(dd), norm.cdf(aa), norm.cdf(bb), norm.cdf(aa))
-            multiplier1 = x0_minus_x1_all_over_x2_minus_x3(norm.pdf(dd), norm.pdf(aa), norm.cdf(dd), norm.cdf(aa))
-            return multiplier0 * (strike - mu + sigma * multiplier1) - premium
+            diff_cdf_bb_aa = norm.cdf(bb) - norm.cdf(aa)
+            diff_cdf_dd_aa = norm.cdf(dd) - norm.cdf(aa)
+            diff_pdf_dd_aa = norm.pdf(dd) - norm.pdf(aa)
+            multiplier0 = diff_cdf_dd_aa / diff_cdf_bb_aa
+            multiplier1 = diff_pdf_dd_aa / diff_cdf_bb_aa
+            return multiplier0 * (strike - mu) + multiplier1 * sigma - premium
 
 def expected_return_all_views(option_dict, view_dicts):
     return sum([